df4a87f507819b7af3c8ae23409ecaa27eaf00b8
HaopengSun <sunhaopeng@gmail.com>
2021-01-08 02:37:39 +0000

    pass

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0;
+  for (const number of array){
+    if (Array.isArray(number)){
+      sum += sumItems(number);
+    } else {
+      sum += Number(number);
+    }
+  }
+  return sum;
 }
 
 module.exports = sumItems;
